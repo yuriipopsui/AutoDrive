28dc96b9b42a23686eb1d439728ca5610a4a026f
Yurii Popsui <yp.onward@gmail.com>
2022-12-02 23:32:42 +0200

    minor fix

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { driversReducer } from './reducers/driversReducer';
-import { authReducer } from './reducers/authReducer';
+// import { configureStore } from '@reduxjs/toolkit';
+// import { driversReducer } from './reducers/driversReducer';
+// import { authReducer } from './reducers/authReducer';
 
 
-export default configureStore({
-  reducer: {
-    // drivers: driversReducer,
-    // auth: authReducer
-  }
-})
+// export default configureStore({
+//   reducer: {
+//     // drivers: driversReducer,
+//     // auth: authReducer
+//   }
+// })
